7fb48edde8944b78e61bc39768d7c7f7a3d29efe
r.ryngenen@gmail.com <Maarika5>
2019-02-02 12:41:22 +0200

    First commit

---
+++ src/main/java/ee/ituk/omniva/OmnivaApplication.java
@@ -0,0 +1,14 @@
+package ee.ituk.omniva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnivaApplication.class, args);
+	}
+
+}
+
